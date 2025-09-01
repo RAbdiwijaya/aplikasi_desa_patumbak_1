f48ddece3915d012673dccffa8df889b690dfbcb
RAbdiwijaya <rifqiabdiw@gmail.com>
2025-09-01 16:19:25 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,11 +16,13 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @production
-        <style>
-            {{-- Load CSS secara inline untuk menghindari flash unstyled content --}}
-            {{ file_get_contents(public_path('build/assets/app.css')) }}
-        </style>
+        {{-- Production fallback --}}
+    @production
+        @php
+            $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+        @endphp
+        <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}">
+        <script type="module" src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}"></script>
     @endproduction
     </head>
     <body class="font-sans antialiased">
