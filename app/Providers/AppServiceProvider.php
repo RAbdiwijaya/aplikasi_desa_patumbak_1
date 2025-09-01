d1a5157f8e9ba83663fc2969c342877cfa4fa0c7
RAbdiwijaya <rifqiabdiw@gmail.com>
2025-09-01 15:42:54 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,25 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        // Fallback untuk assets di production
+        Vite::useStyleTagAttributes(function (string $src, string $url, array $chunk, array $manifest) {
+            if ($this->app->isProduction()) {
+                return [
+                    'rel' => 'stylesheet',
+                    'href' => $url,
+                    'nonce' => request()->header('X-Nonce'),
+                ];
+            }
+            
+            return [];
+        });
+    }
     /**
      * Register any application services.
      */
