71a7a27c754c3703de8285c9d66d4cd6fe044be1
RAbdiwijaya <rifqiabdiw@gmail.com>
2025-09-05 10:11:38 +0700

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,28 +1,33 @@
 import forms from "@tailwindcss/forms";
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
-    "./storage/framework/views/*.php",
+    "./resources/**/*.ts",
+    "./resources/**/*.tsx",
+    "./storage/framework/views/**/*.php",
     "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./vendor/laravel/jetstream/**/*.blade.php",
+    "./vendor/livewire/**/*.blade.php",
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: [
-          'Figtree',
-          'ui-sans-serif',
-          'system-ui',
-          'sans-serif',
-          'Apple Color Emoji',
-          'Segoe UI Emoji',
-          'Segoe UI Symbol',
-          'Noto Color Emoji'
+          "Figtree",
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
         ],
       },
     },
   },
-  plugins: [require('@tailwindcss/forms'),],
+  plugins: [forms],
 };
