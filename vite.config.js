d52fe2591a7defaae7c2b7e890e23abc56185ed7
RAbdiwijaya <rifqiabdiw@gmail.com>
2025-09-05 10:37:10 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-    tailwindcss(),
-  ],
-  build: {
-    outDir: 'public/build',
-    manifest: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name]-[hash].js`,
-        chunkFileNames: `assets/[name]-[hash].js`,
-        assetFileNames: `assets/[name]-[hash].[ext]`
-      }
-    }
-  }
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
 });
